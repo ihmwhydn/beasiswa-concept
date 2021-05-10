4049a571ce29e49bdf7a0d06d9e150f0833b9aa2
ihmwhydn <ihm245@gmail.com>
2021-05-10 20:17:18 +0700

    add file

---
+++ js/myjs.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    AOS.init();
+
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })    
+
+  });
